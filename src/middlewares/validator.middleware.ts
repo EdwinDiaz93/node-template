dd1c419a0c1a10f2309fc7a1255391a4e1c82054
EdwinDiaz93 <josephph1993@gmail.com>
2023-07-24 21:46:12 -0600

    deleted console.log

--- src/middlewares/validator.middleware.ts
+++ src/middlewares/validator.middleware.ts
@@ -9,7 +9,6 @@ const validatorSchema = (schema: Joi.Schema) => (req: Request, res: Response, ne
         next();
     } else {
         const { details } = error;
-        console.log(JSON.stringify(error));
         const errors = details.map(error => ({ key: error.context.key, message: error.message}));
 
         res.status(422).json(errors)
